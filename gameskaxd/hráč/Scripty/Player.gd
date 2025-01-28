9d7de376df27a74481e6e90673d8ce46e90587b7
fckts-mf <tzdilna@gmail.com>
2025-01-28 21:08:33 +0100

    zkouším animaci xd

--- gameskaxd/hráč/Scripty/Player.gd
+++ gameskaxd/hráč/Scripty/Player.gd
@@ -1,8 +1,12 @@
 class_name Player extends CharacterBody2D
 
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
 var move_speed : float = 100.0
+var state : String = "idle"
 
-
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var sprite : Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,14 +16,35 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	velocity = direction * move_speed
+	
+	UpdateAnimation()
 	
 	pass
 
 
 func _physics_process(delta):
 	move_and_slide()
+
+func SetDirection() -> bool:
+	
+	return true
+
+func SetState() -> bool:
+	
+	return true
+
+func UpdateAnimation() -> void:
+	animation_player.play(state + "_" +  Direction())
+	pass
+
+func Direction() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
